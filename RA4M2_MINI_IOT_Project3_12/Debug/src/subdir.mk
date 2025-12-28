323494b736759c34440d97ea14caf5b2d0abef2c
CoreMaker <a845656974@outlook.com>
2025-12-28 21:16:07 +0800

    update

---
+++ RA4M2_MINI_IOT_Project3_12/Debug/src/subdir.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/dht11.c \
+../src/hal_entry.c \
+../src/oled.c 
+
+C_DEPS += \
+./src/dht11.d \
+./src/hal_entry.d \
+./src/oled.d 
+
+OBJS += \
+./src/dht11.o \
+./src/hal_entry.o \
+./src/oled.o 
+
+SREC += \
+RA4M2_MINI_IOT_Project3_12.srec 
+
+MAP += \
+RA4M2_MINI_IOT_Project3_12.map 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	$(file > $@.in,-mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-strict-aliasing -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal -g -D_RENESAS_RA_ -D_RA_CORE=CM33 -D_RA_ORDINAL=1 -I"C:/Users/a8456/e2_studio/workspace/RA4M2_MINI_IOT_Project3_12/src" -I"." -I"C:/Users/a8456/e2_studio/workspace/RA4M2_MINI_IOT_Project3_12/ra/fsp/inc" -I"C:/Users/a8456/e2_studio/workspace/RA4M2_MINI_IOT_Project3_12/ra/fsp/inc/api" -I"C:/Users/a8456/e2_studio/workspace/RA4M2_MINI_IOT_Project3_12/ra/fsp/inc/instances" -I"C:/Users/a8456/e2_studio/workspace/RA4M2_MINI_IOT_Project3_12/ra/arm/CMSIS_6/CMSIS/Core/Include" -I"C:/Users/a8456/e2_studio/workspace/RA4M2_MINI_IOT_Project3_12/ra_gen" -I"C:/Users/a8456/e2_studio/workspace/RA4M2_MINI_IOT_Project3_12/ra_cfg/fsp_cfg/bsp" -I"C:/Users/a8456/e2_studio/workspace/RA4M2_MINI_IOT_Project3_12/ra_cfg/fsp_cfg" -I"C:/Users/a8456/e2_studio/workspace/RA4M2_MINI_IOT_Project3_12/HUAWEI_ESP8266" -std=c99 -Wno-stringop-overflow -Wno-format-truncation --param=min-pagesize=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
+	@echo Building file: $< && arm-none-eabi-gcc @"$@.in"
+
---
+++ RA4M2_MINI_IOT_Project3_12/src/dht11.h
@@ -0,0 +1,20 @@
+/*
+ * DHT11.h
+ *
+ *  Created on: 2023年6月29日
+ *      Author: a8456
+ */
+
+#ifndef DHT11_H_
+#define DHT11_H_
+
+#include "hal_data.h"
+
+
+uint8_t DHT11_ReadByte(void);
+uint8_t DHT11_Read(void);
+
+
+
+#endif /* DHT11_H_ */
+
